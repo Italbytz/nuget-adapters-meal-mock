abf9ed0030880ba92feebaf9237852535646bdc1
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-01-21 11:26:57 +0100

    0.2.0

---
+++ Italbytz.Adapters.Meal.Mock/Italbytz.Adapters.Meal.Mock.Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Italbytz.Ports.Meal;
+using NUnit.Framework;
+
+namespace Italbytz.Adapters.Meal.Mock.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task TestDataSource()
+    {
+        var dataSource = new MockMealDataSource();
+        var meals = await dataSource.RetrieveAll();
+        Assert.AreEqual(3, meals.Count);
+    }
+
+    [Test]
+    public async Task TestGetMealsService()
+    {
+        var service = new MockGetMealsService();
+        var query = new MockMealQuery() { Mensa = 42, Date = DateTime.Now };
+        var meals = await service.Execute(query);
+        Assert.AreEqual(2, meals.Count);
+    }
+}
+
+internal class MockMealQuery : IMealQuery
+{
+    public MockMealQuery()
+    {
+    }
+
+    public int Mensa { get; set; }
+    public DateTime Date { get; set; }
+}
+
---
+++ Italbytz.Adapters.Meal.Mock/Italbytz.Adapters.Meal.Mock/MockMealDataSource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Italbytz.Ports.Common;
+using Italbytz.Ports.Meal;
+
+namespace Italbytz.Adapters.Meal.Mock
+{
+    public class MockMealDataSource : IDataSource<int, IMeal>
+    {
+        public MockMealDataSource()
+        {
+        }
+
+        public Task<IMeal> Retrieve(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<IMeal>> RetrieveAll()
+        {
+            var list = Mocks.Dishes;
+            list.AddRange(Mocks.Desserts);
+            return list;
+        }
+
+    }
+
+}
+
